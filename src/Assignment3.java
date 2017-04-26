602333346b5fac2f6240709bd5ab08328a4e2920
SamMolloy <sammolloy10@gmail.com>
2017-04-26 15:05:28 +0100

    Importing Processing Libraries

---
+++ src/Assignment3.java
@@ -0,0 +1,23 @@
+import processing.core.*;
+
+public class Assignment3 extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main("Assignment3");
+
+	}
+	
+    public void settings(){
+    	size(500,500);
+
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+
+}
