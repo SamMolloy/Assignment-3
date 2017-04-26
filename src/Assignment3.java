77fd9085f338c703cf19ca39322d64276b9baa48
SamMolloy <sammolloy10@gmail.com>
2017-04-26 15:25:04 +0100

    Added collision functionality + more for goal

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -325,7 +325,33 @@ public class Assignment3 extends PApplet{
 	        text(s1, 385, 490);
 	      }
 	      
-	     
+	      void collide()
+	      {
+	        //checks the distance between the player and the green object
+	        float distance = dist(player.x, player.y, collect.randomX, collect.randomY);
+	        
+	        if(distance <= 35)
+	        {
+	          //resets the green objects position
+	          collect.randomX = random(40, 460);
+	          collect.randomY = random(40, 460);
+	          collect.randMove = random(1);
+	          
+	          score++;
+	          
+	         float rand = random(1);
+	         //if score is above 3 it will add either a normal enemy or a smaller and faster orange enemy
+	          if(rand < 0.5)
+	          {
+	            enemies.add(new Enemy2()); 
+	          }//end if
+	          else
+	          {
+	           enemies.add(new Enemy());
+	          }//end else
+	        }//end outer if
+	        
+	      }//end collide
 	      
 	    }//end Score
       
